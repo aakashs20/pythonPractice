6eb92bd450d2b6c610b3e9c9dc882f7c0e62a360
aakashs20 <aakashs20>
2022-03-31 19:13:00 +0530

    Initial commit

---
+++ pythonCoding.py
@@ -0,0 +1,38 @@
+
+
+################## Sort the numbers #############################
+nums = [2, 5, 1, 10, 3]
+print(nums)
+
+nums.sort()
+print(nums)
+nums.sort(reverse=True)
+print(nums)
+
+names = ["Gaurav", "Aakash", "Sonu", "Pooja", "Neha"]
+
+names.sort()
+print(names)
+
+############### Range #################################
+rn = range(6, 15, 2)
+for r in rn:
+    print(r, end=", ")
+
+######################## Reverse Number List ############################
+numbs = [1,5,6,9,0]
+
+for i in range(len(numbs)):
+  for j in range(i+1, len(numbs)):
+    if numbs[i] > numbs[j]:
+        # numbs[i], numbs[j] = numbs[j], numbs[i]
+        temp = numbs[i]
+        temp1= numbs[j]
+        numbs[i] = numbs[j]
+        numbs[j] = temp
+print(numbs)
+
+string = 'abcd'
+
+
+
